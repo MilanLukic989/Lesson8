db621b8b7e74d8c8507aab2ee61d4bc30020ea13
MilanLukic989 <lukic81@gmail.com>
2019-06-10 02:01:49 +0200

    Done

--- src/exercises/JobCandidate.java
+++ src/exercises/JobCandidate.java
@@ -1,6 +1,8 @@
 package exercises;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 /* 
  * 1. Make the JobCandidate class implement Comparable.  
@@ -10,23 +12,44 @@ import java.util.Arrays;
 
 import java.util.List;
 
-public class JobCandidate {
+public class JobCandidate implements Comparable<JobCandidate> {
 
+	private String name;
 	private List<String> personalityTraits;
 	private int hoursSpentCoding;
 	private String teacherName;
 	private int salaryRequired;
 
-	public JobCandidate(List<String> personalityTraits, int hoursSpentCoding, String teacherName, int salaryRequired) {
+	public JobCandidate(List<String> personalityTraits, int hoursSpentCoding, String teacherName, int salaryRequired,
+			String name) {
 		super();
+		this.name = name;
 		this.personalityTraits = personalityTraits;
 		this.hoursSpentCoding = hoursSpentCoding;
 		this.teacherName = teacherName;
 		this.salaryRequired = salaryRequired;
 	}
-	
+
 	public static void main(String[] args) {
-		JobCandidate june = new JobCandidate(Arrays.asList(new String[] {"adaptable", "scatty", "curious"}), 650, "Vinny Wade", 1200000); 
+		JobCandidate june = new JobCandidate(Arrays.asList(new String[] { "adaptable", "scatty", "curious" }), 650,
+				"Vinny Wade", 1200000, "June Clark");
+		JobCandidate milan = new JobCandidate(Arrays.asList(new String[] { "adaptable", "scatty", "curious" }),
+				june.hoursSpentCoding/3, "June Clark", june.salaryRequired*2, "Milan Lukic");
+
+		ArrayList<JobCandidate> candidatesList = new ArrayList<JobCandidate>();
+		candidatesList.add(june);
+		candidatesList.add(milan);
+
+		System.out.println("Job should be given to : " + candidatesList.get(0).name);
+	}
+
+	@Override
+	public int compareTo(JobCandidate obj) {
+
+		int compare = this.personalityTraits.size() - obj.personalityTraits.size();
+		compare += this.hoursSpentCoding - obj.hoursSpentCoding;
+		compare += this.salaryRequired - obj.salaryRequired;
+		return compare;
 	}
 
 }
--- src/exercises/MovieProducer.java
+++ src/exercises/MovieProducer.java
@@ -2,6 +2,7 @@ package exercises;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MovieProducer {
 
@@ -12,10 +13,62 @@ public class MovieProducer {
 	 */
 
 	public static void main(String[] args) {
-//		List<Snake> snakes = SnakeFactory.createListOfSnakesWithThatAreRandomlyVenomous();
-//		Plane plane = new Plane();
-//		plane.addSnakes(snakes);
-//		int howManyCanKillMe = plane.getNumberOfVenomousSnakes();
-//		System.out.println("Sam has to kill the right " + howManyCanKillMe + " snakes.");
+		List<Snake> snakes = SnakeFactory.createListOfSnakesWithThatAreRandomlyVenomous();
+		Plane plane = new Plane();
+		plane.addSnakes(snakes);
+		int howManyCanKillMe = plane.getNumberOfVenomousSnakes();
+		System.out.println("Sam has to kill the right " + howManyCanKillMe + " snakes.");
+	}
+}
+
+ class Snake {
+
+	private boolean venomous;
+
+	public Snake(boolean venomous) {
+		this.venomous = venomous;
+
+	}
+
+	public boolean getVenomous() {
+		return venomous;
+	}
+
+	public void setVenomous(boolean venomous) {
+		this.venomous = venomous;
+	}
+
+}
+ 
+class Plane {
+	
+		private List<Snake> snakes;
+
+		public void addSnakes(List<Snake> snakes) {
+			this.snakes = snakes;
+		}
+
+		public int getNumberOfVenomousSnakes() {
+			int numOfSnakes = 0;
+
+			for (Snake snake : snakes) {
+				if (snake.getVenomous()) {
+					numOfSnakes++;
+				}
+			}
+			return numOfSnakes;
+		}
+	}
+
+class SnakeFactory{
+
+	public static List<Snake> createListOfSnakesWithThatAreRandomlyVenomous() {
+		boolean venomous = new Random().nextBoolean();
+		List<Snake> snakes = new ArrayList<Snake>();
+		for (int i = 0; i < 10000; i++) {
+			snakes.add(new Snake(venomous));
+			venomous = new Random().nextBoolean();
+		}
+		return snakes;
 	}
 }
